a16e0aa9fd078fa45aaf8b837483e293e87d4f94
KhadijaMahanga <khadijamahanga@y7mail.com>
2025-02-20 12:36:42 +0300

    practice 20th feb

--- python_practise/longestSubstring/index.py
+++ python_practise/longestSubstring/index.py
@@ -3,19 +3,13 @@ class Solution:
         lenght_long_string = 0
         
         sub = ""
-        i = 0
-        while i < len(s):
-            if s[i] in sub:
-                if len(sub) > lenght_long_string:
-                    lenght_long_string = len(sub)
-                
-                s = s[s.index(s[i]) + 1:]
-                sub = ""
-                i = 0
+        for c in s:
+            if c in sub:
+                sub = sub[sub.index(c) + 1:]
             else:
-                sub += s[i]
-                i+= 1
-        if len(sub) > lenght_long_string:
+                sub += c
+        
+            if len(sub) > lenght_long_string:
                     lenght_long_string = len(sub)
 
         return lenght_long_string
@@ -24,16 +18,16 @@ class Solution:
 
 # best solution
 # class Solution:
-    def lengthOfLongestSubstring(self, s: str) -> int:
-        char_set = set()
-        left = max_len = 0
+    # def lengthOfLongestSubstring(self, s: str) -> int:
+    #     char_set = set()
+    #     left = max_len = 0
 
-        for right in range(len(s)):
-            while s[right] in char_set:
-                char_set.remove(s[left])
-                left += 1
+    #     for right in range(len(s)):
+    #         while s[right] in char_set:
+    #             char_set.remove(s[left])
+    #             left += 1
 
-            char_set.add(s[right])
-            max_len = max(max_len, right - left + 1)
+    #         char_set.add(s[right])
+    #         max_len = max(max_len, right - left + 1)
 
-        return max_len        
+    #     return max_len        
